bda343401f42a8b9d1b97b88bf836e3a881e944d
shonb6570 <47017803+shonb6570@users.noreply.github.com>
2020-12-02 12:45:49 -0500

    restructuring html and js - see notes
    Multiple changes to bootstrap classes and in css.
    
    refactoring JS to use intersection observer for all aniamtions.

--- script.js
+++ script.js
@@ -17,57 +17,61 @@ const projectTitleWrapper = document.getElementById('projectTitleWrapper');
 const mountains = document.getElementById('mountains');
 
             // individual project elements
-const awesomeCo = document.querySelectorAll(".awesomeCo");
-const drumSpace = document.querySelectorAll(".drumSpace");
-const dataAnalytics = document.querySelectorAll(".dataAnalytics");
-const styleGuide = document.querySelectorAll(".styleGuide");
-const fsc2020 = document.querySelectorAll(".fsc2020");
-const lightBox = document.querySelectorAll(".lightBox");
+// const awesomeCo = document.querySelectorAll(".awesomeCo");
+// const drumSpace = document.querySelectorAll(".drumSpace");
+// const dataAnalytics = document.querySelectorAll(".dataAnalytics");
+// const styleGuide = document.querySelectorAll(".styleGuide");
+// const fsc2020 = document.querySelectorAll(".fsc2020");
+
+
+
+const graphicDesignHeadline = document.getElementById("graphicDesignHeadline");
+
 
 
 
 /**
  * Window onload play order for embedded videos
  */
-window.onload = function(){ 
-    vid1.addEventListener("ended", function(){ 
-        playVideo("vid2"); 
-        pause(vid1); // add display "none" to video div after end
-        play(vid2); // make the next video div visible
-    });
+// window.onload = function(){ 
+//     vid1.addEventListener("ended", function(){ 
+//         playVideo("vid2"); 
+//         pause(vid1); // add display "none" to video div after end
+//         play(vid2); // make the next video div visible
+//     });
     
     
-    vid2.addEventListener("ended", function(){ 
-        playVideo("vid3"); 
-        pause(vid2);
-        play(vid3);
-    });
-    vid3.addEventListener("ended", function(){ 
-        playVideo("vid1"); 
-        pause(vid3);
-        play(vid1);
-    });    
-}
+//     vid2.addEventListener("ended", function(){ 
+//         playVideo("vid3"); 
+//         pause(vid2);
+//         play(vid3);
+//     });
+//     vid3.addEventListener("ended", function(){ 
+//         playVideo("vid1"); 
+//         pause(vid3);
+//         play(vid1);
+//     });    
+// }
 /**
  * Plays video based on ID of a video element
  */
 
-const playVideo = (videoID) => {
-    const videoElement = document.getElementById(videoID);
-    videoElement.muted = true;
-    videoElement.play();
-    videoElement.classList.remove("paused"); 
-}
+// const playVideo = (videoID) => {
+//     const videoElement = document.getElementById(videoID);
+//     videoElement.muted = true;
+//     videoElement.play();
+//     videoElement.classList.remove("paused"); 
+// }
 
-const pause = (id) => {
-    id.classList.remove("video");
-    id.classList.add("paused");
-}
+// const pause = (id) => {
+//     id.classList.remove("video");
+//     id.classList.add("paused");
+// }
 
-const play = (id) => {
-    id.classList.remove("paused");
-    id.classList.add("video");
-}
+// const play = (id) => {
+//     id.classList.remove("paused");
+//     id.classList.add("video");
+// }
 
 //Quotes (for landing/main page)
 
@@ -119,27 +123,105 @@ const hide = "hide .1s ease-in-out forwards";
 // slide function takes the current scroll value (position on page) and the element targeted, 
 // and adds an animation
 
-const animate = (scrollValue, element, animationOne, animationTwo) => {
-    const distance = window.scrollY;
-    if(distance >= scrollValue){
-            element.style.animation = animationOne;
-        } else {
-            element.style.animation = animationTwo;
-    }
-}
+// const animate = (scrollValue, element, animationOne, animationTwo) => {
+//     const distance = window.scrollY;
+//     if(distance >= scrollValue){
+//             element.style.animation = animationOne;
+//         } else {
+//             element.style.animation = animationTwo;
+//     }
+// }
 
-const animate2 = (scrollValue, el, animationOne, animationTwo) => {
-    const distance = window.scrollY;
-    if(distance >= scrollValue){
-        el.forEach(element => {
-            element.style.animation = animationOne;
-        }); 
-    } else {
-        el.forEach(element => {
-            element.style.animation = animationTwo;
-        }); 
-    }
-}
+// const animate2 = (scrollValue, el, animationOne, animationTwo) => {
+//     const distance = window.scrollY;
+//     if(distance >= scrollValue){
+//         el.forEach(element => {
+//             element.style.animation = animationOne;
+//         }); 
+//     } else {
+//         el.forEach(element => {
+//             element.style.animation = animationTwo;
+//         }); 
+//     }
+// }
+
+
+//IntersectionObserver loads element animations when in view
+
+let animationElement1;
+let animationElement2;
+let animationElement3;
+let animationElement4;
+let animationElement5;
+let animationElement6;
+let animationElement7;
+
+let prevRatio = 0.0;
+
+window.addEventListener("load", (event) => {
+    
+  animationElement1 = document.getElementById("graphicDesignSection");
+  animationElement2 = document.getElementById("about");
+  animationElement3 = document.getElementById("animateProjectContainer1");
+  animationElement4 = document.getElementById("animateProjectContainer2");
+  animationElement5 = document.getElementById("animateProjectContainer3");
+  animationElement6 = document.getElementById("animateProjectContainer4");
+  animationElement7 = document.getElementById("animateProjectContainer5");
+
+  createObserver();
+}, false);
+
+
+function createObserver() {
+    let observer;
+  
+    let options = {
+      root: null,
+      //margin of space above and below item before item is observed(before callback animation is fired)
+      rootMargin: "400px 50px",
+      threshold: [1]
+    };
+  
+    observer = new IntersectionObserver(handleIntersect, options);
+    observer.observe(animationElement1);
+    observer.observe(animationElement2);
+    observer.observe(animationElement3);
+    observer.observe(animationElement4);
+    observer.observe(animationElement5);
+    observer.observe(animationElement6);
+    observer.observe(animationElement7);
+  }
+
+
+  function handleIntersect(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio === 1) {
+        entry.target.firstElementChild.style.animation = leftAnimation2;
+      } else {
+        entry.target.firstElementChild.style.animation = rightAnimation2;
+      }
+  
+      prevRatio = entry.intersectionRatio;
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -147,64 +229,52 @@ const animate2 = (scrollValue, el, animationOne, animationTwo) => {
 
 //parallax slide animation for elements on page
 
-window.addEventListener("scroll", function() {
+// window.addEventListener("scroll", function() {
 
-    const mobile = 575;
-    const desktop = 1100;
-    animate(201, aboutTitle, leftAnimation2, rightAnimation2);
-    animate(950, navBrand, appear, hide);
+//     const mobile = 575;
+//     const desktop = 1100;
+//     animate(201, aboutTitle, leftAnimation2, rightAnimation2);
+//     animate(950, navBrand, appear, hide);
+//     animate(1700, graphicDesignHeadline, leftAnimation2, rightAnimation2);
 
     // slide in projects on scroll
-    if (window.innerWidth <= mobile) {
-        animate(7800, projects, moveUpBG, moveDownBG);
-        animate(7800, mountains, moveRightBG, moveLeftBG);    
-        animate2(8000, awesomeCo, rightAnimation, leftAnimation);
-        animate2(8300, drumSpace, leftAnimation2, rightAnimation2);
-        animate2(8950, dataAnalytics, rightAnimation, leftAnimation);
-        animate2(9550, styleGuide, leftAnimation2, rightAnimation2);
-        animate2(10050, fsc2020, rightAnimation, leftAnimation);
-        animate2(10650, lightBox, leftAnimation2, rightAnimation2);
-    } else if (window.innerWidth > mobile && window.innerWidth < desktop ) {
-        animate(7600, projects, moveUpBG, moveDownBG);
-        animate(7600, mountains, moveRightBG, moveLeftBG);    
-        animate2(8000, awesomeCo, rightAnimation, leftAnimation);
-        animate2(8300, drumSpace, leftAnimation2, rightAnimation2);
-        animate2(8950, dataAnalytics, rightAnimation, leftAnimation);
-        animate2(9550, styleGuide, leftAnimation2, rightAnimation2);
-        animate2(10050, fsc2020, rightAnimation, leftAnimation);
-        animate2(10650, lightBox, leftAnimation2, rightAnimation2);
-    } else {
-        animate(7800, projects, moveUpBG, moveDownBG);
-        animate(7800, mountains, moveRightBG, moveLeftBG);    
-        animate2(8000, awesomeCo, rightAnimation, leftAnimation);
-        animate2(8300, drumSpace, leftAnimation2, rightAnimation2);
-        animate2(8650, dataAnalytics, rightAnimation, leftAnimation);
-        animate2(8950, styleGuide, leftAnimation2, rightAnimation2);
-        animate2(9450, fsc2020, rightAnimation, leftAnimation);
-        animate2(9750, lightBox, leftAnimation2, rightAnimation2);
-    } 
-});
+    // if (window.innerWidth <= mobile) {
+
+        // graphicDesignSectionContainer.style.display = "none";
+
+        // animate(2700, projects, moveUpBG, moveDownBG);
+        // animate(2700, mountains, moveRightBG, moveLeftBG);    
+        // animate2(2900, awesomeCo, leftAnimation2, rightAnimation2);
+        // animate2(3200, drumSpace, leftAnimation2, rightAnimation2);
+        // animate2(3950, dataAnalytics, leftAnimation2, rightAnimation2);
+        // animate2(4350, styleGuide, leftAnimation2, rightAnimation2);
+        // animate2(4550, fsc2020, leftAnimation2, rightAnimation2);
+
+//    } else if (window.innerWidth > mobile && window.innerWidth < desktop ) {
+
+        // graphicDesignSectionContainer.style.display = "block";
 
 
-gsap.registerPlugin(ScrollTrigger);
+//         animate(2700, projects, moveUpBG, moveDownBG);
+//         animate(2700, mountains, moveRightBG, moveLeftBG);    
+//         animate2(2900, awesomeCo, leftAnimation2, rightAnimation2);
+//         animate2(3200, drumSpace, leftAnimation2, rightAnimation2);
+//         animate2(3950, dataAnalytics, leftAnimation2, rightAnimation2);
+//         animate2(4350, styleGuide, leftAnimation2, rightAnimation2);
+//         animate2(4550, fsc2020, leftAnimation2, rightAnimation2);
 
-let sections = gsap.utils.toArray(".slide");
+//     } else {
+//         animate(2700, projects, moveUpBG, moveDownBG);
+//         animate(2700, mountains, moveRightBG, moveLeftBG);    
+//         animate2(2900, awesomeCo, leftAnimation2, rightAnimation2);
+//         animate2(3200, drumSpace, leftAnimation2, rightAnimation2);
+//         animate2(3950, dataAnalytics, leftAnimation2, rightAnimation2);
+//         animate2(4350, styleGuide, leftAnimation2, rightAnimation2);
+//         animate2(4550, fsc2020, leftAnimation2, rightAnimation2);
 
-var tl = gsap.timeline({x: 100, duration: 3});
 
-tl.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".img-slider",
-    start: "bottom 80%",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + document.querySelector(".img-slider").offsetWidth
-  }
-});
+//     } 
+// });
 
 
 
